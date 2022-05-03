f57dfda4eb4d914c9bcac3f3c9195473cc8457ee
Zz <oliani748@gmail.com>
2022-05-03 11:28:59 -0300

    Initial commit

---
+++ bankline-api/src/main/java/Model/Conta.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Conta {
+
+    private Long number;
+    private Double balance;
+    
+    public Long getNumber() {
+        return number;
+    }
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+    public Double getBalance() {
+        return balance;
+    }
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+    
+    
+}
---
+++ bankline-api/src/main/java/Model/MovimentaçãoTipo.java
@@ -0,0 +1,8 @@
+package Model;
+
+public enum MovimentaçãoTipo {
+
+    INCOME,
+    EXPENSE
+    
+}
